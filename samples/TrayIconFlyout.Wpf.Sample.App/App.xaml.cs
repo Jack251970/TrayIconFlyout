1e7957e2ac8c921fc922185576a9bf6262bf50a8
Jack251970 <1160210343@qq.com>
2026-01-22 16:11:02 +0800

    Refactor app startup and add tray icon management
    Removed StartupUri from App.xaml and now create MainWindow programmatically in App.xaml.cs. Introduced tray icon initialization and cleanup via TrayIconManager. Ensured proper resource disposal on app exit and added copyright/license header.

--- samples/TrayIconFlyout.Wpf.Sample.App/App.xaml.cs
+++ samples/TrayIconFlyout.Wpf.Sample.App/App.xaml.cs
@@ -1,11 +1,36 @@
-﻿using System.Windows;
+﻿// Copyright (c) Jack251970. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.Windows;
 
 namespace U5BFA.Libraries
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
-	}
+        private Window? _window;
+
+        public App()
+        {
+            InitializeComponent();
+
+            AppDomain.CurrentDomain.ProcessExit += AppDomain_ProcessExit;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            TrayIconManager.Default.Initialize(new(
+                "Assets\\Tray.ico",
+                "TrayIconFlyout sample app (WPF)",
+                new("056EACEE-82B0-48AC-A6E9-34DAE5CD37F3")));
+
+            _window = new MainWindow();
+            _window.Activate();
+        }
+
+        private void AppDomain_ProcessExit(object? sender, EventArgs e)
+        {
+            TrayIconManager.Default.Dispose();
+        }
+    }
 }
